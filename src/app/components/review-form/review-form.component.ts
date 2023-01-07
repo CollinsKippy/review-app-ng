a977a69921d72d39264371a29ee43f8194657022
CK <collins@analence.com>
2023-01-07 21:05:27 +0300

    updated form component

--- src/app/components/review-form/review-form.component.ts
+++ src/app/components/review-form/review-form.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-review-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './review-form.component.html',
-  styleUrls: ['./review-form.component.scss']
+  styleUrls: ['./review-form.component.scss'],
 })
 export class ReviewFormComponent {
+  reviewForm: FormGroup = new FormGroup({
+    id: new FormControl(),
+    comment: new FormControl(''),
+    rating: new FormControl(),
+  });
 
+  ratings: number[] = Array.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 }
